92640d9bc70acda4bbad25ba8e681597511dcde6
Abhishek Kaisar Abhoy <84991791+AbhishekKaisar@users.noreply.github.com>
2024-02-05 18:08:33 +0600

    Add files via upload

---
+++ AT - Toy Army/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number_of_soldiers, killed;
+    scanf("%d", &number_of_soldiers);
+
+    killed = 3*((number_of_soldiers + 1)/2);
+    printf("%d\n", killed);
+    return 0;
+}
